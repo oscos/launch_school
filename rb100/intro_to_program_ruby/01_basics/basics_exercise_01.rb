cf367ac8e7577546f998e3bc406a2fc9fab537e2
Oscar Cortes <cortes.oscar@outlook.com>
2019-11-08 05:04:29 +0000

    add basics_exercise_01.rb

---
+++ rb100/intro_to_program_ruby/01_basics/basics_exercise_01.rb
@@ -0,0 +1,13 @@
+# Launch School Book: Introduction To Programming
+# The Basics - Excercise 01 
+
+=begin
+Add two strings together that, when concatenated, 
+return your first and last name as your full name in one string.
+=end
+
+first_name = 'Jason'
+last_name = 'Bourne'
+
+puts "Print name using string interpolation: #{first_name} #{last_name}"
+puts "Print name using string concatenation: " +  first_name + " " + last_name
