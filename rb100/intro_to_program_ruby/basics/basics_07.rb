334434f866854f4480a3ff0073228eef4cb247c7
Oscar Cortes <cortes.oscar@outlook.com>
2019-11-09 22:54:23 +0000

    message added

---
+++ rb100/intro_to_program_ruby/basics/basics_07.rb
@@ -0,0 +1,8 @@
+# Launch School Book: Introduction To Programming With Ruby
+# The Basics - Excercise 07 
+# Answered On: 11/09/2019
+
+# INSTRUCTIONS:
+# What does the following error message tell you?
+
+puts "Syntax error due to typo: parenthesis \) was used instead of a closing curly brace \} or missing curtly brace all toghether."
