c3f6858228b68d4b11022dd841afde36cb410a05
Oscar Cortes <cortes.oscar@outlook.com>
2020-03-14 21:48:51 +0000

    first commit

---
+++ rb100/intro_ruby/exercises/exercise_15.rb
@@ -0,0 +1,19 @@
+# Launch School Book: Introduction To Programming With Ruby
+# exercises_15.rb 
+# Answered On: 03/14/2020
+
+# INSTRUCTIONS:
+# What will the following program output?
+
+=begin
+hash1 = {shoes: "nike", "hat" => "adidas", :hoodie => true}
+hash2 = {"hat" => "adidas", :shoes => "nike", hoodie: true}
+
+if hash1 == hash2
+  puts "These hashes are the same!"
+else
+  puts "These hashes are not the same!"
+end
+=end
+
+puts "These hases are the same!"
