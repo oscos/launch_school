91a441d76c595022108ee1b4e9e981ebdd0a8cd4
Oscar Cortes <cortes.oscar@outlook.com>
2020-06-06 22:56:23 +0000

    first commit

--- rb100/ruby_basics/loops1/ex.rb
+++ rb100/ruby_basics/loops1/ex.rb
@@ -1,8 +1,11 @@
-# ex04.md
+# ex05.md
+# Modify the code below so "Hello!" is printed 5 times.
 
-loop do
-  puts 'Should I stop looping?'
-  answer = gets.chomp.downcase
-  break if answer == "yes"
-  puts "enter the word 'yes' to stop loop."
+say_hello = true
+i = 0
+
+while say_hello
+  puts 'Hello!'
+  i += 1
+  say_hello = false if i == 5  
 end
