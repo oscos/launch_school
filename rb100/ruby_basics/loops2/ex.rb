59335a9d6c4cde8f0f6af7c350bbf0da268097ec
Oscar Cortes <cortes.oscar@outlook.com>
2020-06-09 23:27:48 +0000

    first commit ex01.md

---
+++ rb100/ruby_basics/loops2/ex.rb
@@ -0,0 +1,14 @@
+# ex01.md
+
+count = 1
+
+loop do
+   if count.even?
+       puts "#{count} is even!?"
+   else
+       puts "#{count} is odd!"
+   end
+   
+   count += 1   
+   break if count > 5  
+end
