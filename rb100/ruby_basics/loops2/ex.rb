ae2427bb0840e22fb632d5def3ad7a083689b2c2
Oscar Cortes <cortes.oscar@outlook.com>
2020-06-10 01:11:39 +0000

    first commit ex05.md

--- rb100/ruby_basics/loops2/ex.rb
+++ rb100/ruby_basics/loops2/ex.rb
@@ -1,19 +1,15 @@
-# ex04.md
-# Get the Sum
+# ex05.md
+# Insert Numbers
+# Modify the code below so that the user's input gets added to the numbers array. 
+# Stop the loop when the array contains 5 numbers.
 
-# The code below asks the user "What does 2 + 2 equal?" and uses #gets to retrieve 
-# the user's answer. Modify the code so "That's correct!" is printed and the loop 
-# stops when the user's answer equals 4. Print "Wrong answer. Try again!" if the 
-# user's answer doesn't equal 4.
+numbers = []
 
 loop do
-  puts 'What does 2 + 2 equal?'
-  answer = gets.chomp.to_i
+  puts 'Enter any number:'
+  input = gets.chomp.to_i
 
-   if answer == 4
-      puts "That's correct!"  
-      break
-   else 
-      puts "Wrong answer. Try again!"
-   end
+  numbers.push(input)
+  break if numbers.size == 5
 end
+puts numbers
