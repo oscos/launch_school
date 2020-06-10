dbd28649c7755338f3c20b58341bb14f65162380
Oscar Cortes <cortes.oscar@outlook.com>
2020-06-10 17:08:03 +0000

    first commit ex09.md

--- rb100/ruby_basics/loops2/ex.rb
+++ rb100/ruby_basics/loops2/ex.rb
@@ -1,20 +1,25 @@
-# ex07.md
+# ex09.md
 # Only Even
-# Using next, modify the code below so that it only prints even numbers.
+# The following code increments number_a and number_b by either 0 or 1. loop is 
+# used so that the variables can be incremented more than once, however, break 
+# stops the loop after the first iteration. Use next to modify the code so that 
+# the loop iterates until either number_a or number_b equals 5. Print "5 was 
+# reached!" before breaking out of the loop.
 
-number = 0
+number_a = 0
+number_b = 0
 
-# until number == 10
-#   number += 1
-#   if number.odd?
-#     puts number.next
-#   end
-# end
+loop do
+  number_a += rand(2)
+  number_b += rand(2)
 
-number = 0
+  next unless (number_a == 5 || number_b == 5)
+  puts "5 was reached!"
+  break
+  
+  # if (number_a == 5 || number_b == 5)
+  #   puts "5 was reached! -- #{number_a} -- #{number_b}"
+  #   break
+  # end
 
-until number == 10
-  number += 1
-    next if number.odd?
-    puts number
 end
