0b44b4d9f305fcadb2c3f9b8641a939c91042288
Oscar Cortes <cortes.oscar@outlook.com>
2020-06-10 00:13:56 +0000

    first commit ex03.md

--- rb100/ruby_basics/loops2/ex.rb
+++ rb100/ruby_basics/loops2/ex.rb
@@ -1,10 +1,16 @@
-# ex02.md
-# Catch the Number
-# Modify the following code so that the loop stops if number is equal to or between 0 and 10.
+# ex03.md
+# Conditional Loop
+# Using an if/else statement, run a loop that prints "The loop was processed!" one time 
+# if process_the_loop equals true. Print "The loop wasn't processed!" if process_the_loop 
+# equals false.
 
+process_the_loop = [true, false].sample
 
-loop do
-  number = rand(100)
-  puts number
-  break if number.between?(0,10)
+if process_the_loop
+   loop do
+      puts "The loop was processed!"
+      break
+   end
+else
+   puts "The loop wasn't processed!"
 end
