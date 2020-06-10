f8046edf6d3314f6b3722979be113735df1a7e85
Oscar Cortes <cortes.oscar@outlook.com>
2020-06-10 14:07:16 +0000

    first commit ex06.md

--- rb100/ruby_basics/loops2/ex.rb
+++ rb100/ruby_basics/loops2/ex.rb
@@ -1,15 +1,13 @@
-# ex05.md
-# Insert Numbers
-# Modify the code below so that the user's input gets added to the numbers array. 
-# Stop the loop when the array contains 5 numbers.
+# ex06.md
+# Get the Sum
+# Given the array below, use loop to remove and print each name. Stop the loop 
+# once names doesn't contain any more elements.  Keep in mind to only use loop, 
+# not while, until, etc.
 
-numbers = []
+names = ['Sally', 'Joe', 'Lisa', 'Henry']
 
 loop do
-  puts 'Enter any number:'
-  input = gets.chomp.to_i
-
-  numbers.push(input)
-  break if numbers.size == 5
+  puts "Hello, #{names.shift}!" # first to last
+  # puts names.pop # last to first
+  break if names.empty?
 end
-puts numbers
