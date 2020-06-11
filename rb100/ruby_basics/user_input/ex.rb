1f9e32756d83b6c0d80dbca1a8300a50d3d7ac19
Oscar Cortes <cortes.oscar@outlook.com>
2020-06-11 05:02:50 +0000

    first commit ex01.md

---
+++ rb100/ruby_basics/user_input/ex.rb
@@ -0,0 +1,9 @@
+# ex01.md
+# Repeat after me
+
+# Write a program that asks the user to type something in, after which your 
+# program should simply display what was entered.
+
+puts ">> Type something please."
+input = gets.chomp
+puts input
