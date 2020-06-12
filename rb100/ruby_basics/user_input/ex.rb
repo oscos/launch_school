f56bc93898fab25e01ab6d8a273d485a3b6a5e3e
Oscar Cortes <cortes.oscar@outlook.com>
2020-06-12 05:21:07 +0000

    first commit ex06.md

--- rb100/ruby_basics/user_input/ex.rb
+++ rb100/ruby_basics/user_input/ex.rb
@@ -1,26 +1,18 @@
-# ex05.md
-# Launch School Printer (Part 1)
-# Write a program that prints 'Launch School is the best!' repeatedly until a 
-# certain number of lines have been printed. The program should obtain the number 
-# of lines from the user, and should insist that at least 3 lines are printed.
+# ex06.md
+# Passwords
 
-# For now, just use #to_i to convert the input value to an Integer, and check that 
-# result instead of trying to insist on a valid number; validation of numeric 
-# input is a topic with a fair number of edge conditions that are beyond the 
-# scope of this exercise.
+# Write a program that displays a welcome message, but only after the user enters 
+# the correct password, where the password is a string that is defined as a 
+# constant in your program. Keep asking for the password until the user enters 
+# the correct password.
 
-
-user_input = nil
+PASSWORD = 'AbcEasyAs123'
 
 loop do
-  puts ">> How many output lines do you want? Enter a number >= 3:"
-  user_input = gets.chomp.to_i  
-  break if user_input >= 3
-  puts ">> That's not enough lines."  
+  puts ">> Please enter your passord: "
+  user_input = gets.chomp
+  break if user_input == PASSWORD
+  puts "Invalid password!"
 end
-  
-loop do
-  puts "Launch School is the best!"
-  user_input -= 1
-  break if user_input == 0
-end
+
+puts "Welcome!"
