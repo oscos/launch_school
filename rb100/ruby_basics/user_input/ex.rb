ebc8c257b11428ca69a6c7d6c3805d8b47758f8c
Oscar Cortes <cortes.oscar@outlook.com>
2020-06-12 03:07:11 +0000

    first commit ex04.md

--- rb100/ruby_basics/user_input/ex.rb
+++ rb100/ruby_basics/user_input/ex.rb
@@ -1,15 +1,28 @@
-# ex03.md
-# Print Something (Part 1)
+# ex04.md
+# Print Something (Part 2)
 
-# Write a program that asks the user whether they want the program to print 
-# "something", then print it if the user enters y. Otherwise, print nothing.
+# In the previous exercise, you wrote a program that asks the user if they want 
+# the program to print "something". However, this program recognized any input as 
+# valid: if you answered anything but y, it treated it as an n response, and quit 
+# without printing anything.
 
-puts "Do you want this program to print something? Enter `y` for yes."
-user_input = gets.chomp.downcase
+# Modify your program so it prints an error message for any inputs that aren't y or n, 
+# and then asks you to try again. Keep asking for a response until you receive a 
+# valid y or n response. In addition, your program should allow both Y and N 
+# (uppercase) responses; case sensitive input is generally a poor user interface 
+# choice. Whenever possible, accept both uppercase and lowercase inputs.
 
-if user_input == 'y'
-  puts "something"
+
+loop do
+  puts ">> Do you want this program to print something? Enter y or n"
+  user_input = gets.chomp.downcase
+  
+  if user_input == 'y'
+    puts "something"
+    break
+  elsif user_input == 'n'
+    break
+  else 
+    puts ">> Invalid input! Please enter y or n"
+  end
 end
-
-
-
