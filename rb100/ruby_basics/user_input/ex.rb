0cd50603ed5ba66a04a29f0afb15d60cbcd5f2d3
Oscar Cortes <cortes.oscar@outlook.com>
2020-06-14 19:36:25 +0000

    first commit of ex08.md and ex09.md

--- rb100/ruby_basics/user_input/ex.rb
+++ rb100/ruby_basics/user_input/ex.rb
@@ -1,30 +1,38 @@
-# ex07.md
-# User Name and Password
+# ex0.md
+# Launch School Printer (Part 2)
 
-# In the previous exercise, you wrote a program to solicit a password. In this 
-# exercise, you should modify the program so it also requires a user name. The 
-# program should solicit both the user name and the password, then validate both, 
-# and issue a generic error message if one or both are incorrect; the error message 
-# should not tell the user which item is incorrect.
+# In an earlier exercise, you wrote a program that prints 'Launch School is the 
+# best!' repeatedly until a certain number of lines have been printed. Our solution 
+# looked like this:
 
-USERNAME = 'Jackson'
-PASSWORD = 'AbcEasyAs123'
-
-input_pass = nil
-input_user = nil
+# Modify this program so it repeats itself after each input/print iteration, 
+# asking for a new number each time through. The program should keep running 
+# until the user enters q or Q.
 
 loop do
-  puts ">> Please enter your user name: "
-  input_user = gets.chomp
-  break if input_user == USERNAME
-  puts "Authorization failed!"
-end
 
-loop do
-  puts ">> Please enter your password: "
-  input_pass = gets.chomp
-  break if input_pass == PASSWORD
-  puts "Authorization failed!"
-end
+  user_string = nil
+  user_number = nil
 
-puts "Welcome!"
+  loop do
+    puts ">> How many output lines do you want? Enter a number >= 3 (Q to quit):"
+    
+    user_string = gets.chomp.downcase
+    user_number = user_string.to_i
+    
+    break if user_string == 'q'
+    
+    break if user_number >= 3
+    puts ">> That's not enough lines."
+    
+  end
+  
+  break if user_string == 'q'
+  
+  loop do
+    puts "Launch School is the best!"
+    user_number -= 1
+    break if user_number == 0
+  end
+
+end 
