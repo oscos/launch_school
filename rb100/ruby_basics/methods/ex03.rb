edd124c43ec9d470f3ec908eabdf2e403f039c60
Oscar Cortes <cortes.oscar@outlook.com>
2020-06-15 04:17:48 +0000

    first commit ex03

---
+++ rb100/ruby_basics/methods/ex03.rb
@@ -0,0 +1,16 @@
+# ex03.rb
+# Greeting Through Methods (Part 1)
+
+# Write two methods, one that returns the string "Hello" and one that returns the 
+# string "World". Then print both strings using #puts, combining them into one 
+# sentence.
+
+def method_a
+    "Hello"
+end
+
+def method_b
+    "World!"
+end
+
+puts "#{method_a} " " #{method_b}"
