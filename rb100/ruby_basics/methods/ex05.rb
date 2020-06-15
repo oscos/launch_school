2b3d9d07100418a7a3496ae1e710eb73adcb9421
Oscar Cortes <cortes.oscar@outlook.com>
2020-06-15 04:43:07 +0000

    first commit ex05

---
+++ rb100/ruby_basics/methods/ex05.rb
@@ -0,0 +1,11 @@
+# ex05.rb
+# Make and Model
+
+# Using the following code, write a method called car that takes two arguments 
+# and prints a string containing the values of both arguments.
+
+def car(param1, param2)
+    param1 + " " + param2
+end
+
+puts car('Toyota', 'Corolla')
