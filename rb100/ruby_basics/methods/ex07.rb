677968d202f7e78c5c48e1ff57eacadbbb716da4
Oscar Cortes <cortes.oscar@outlook.com>
2020-06-18 20:35:04 -0700

    first commit ex07

---
+++ rb100/ruby_basics/methods/ex07.rb
@@ -0,0 +1,15 @@
+# ex07.rb
+# Naming Animals
+
+# Fix the following code so that the names are printed as expected.
+
+def dog(name)
+    return name
+  end
+  
+  def cat(name)
+    return name
+end
+  
+puts "The dog's name is #{dog('Spot')}."
+puts "The cat's name is #{cat('Spot')}."
