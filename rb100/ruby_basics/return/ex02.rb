4efd303d4770536cd0c0b31fb0a08f7a5120fa22
Oscar Cortes <cortes.oscar@outlook.com>
2020-06-23 08:47:41 -0700

    various corrections

--- rb100/ruby_basics/return/ex02.rb
+++ rb100/ruby_basics/return/ex02.rb
@@ -1,5 +1,5 @@
-# ex01.rb
-# Breakfast, Lunch, or Dinner? (Part 1)
+# ex02.rb
+# Breakfast, Lunch, or Dinner? (Part 2)
 
 # What will the following code print? Why? Don't run it until you've attempted 
 # to answer.
