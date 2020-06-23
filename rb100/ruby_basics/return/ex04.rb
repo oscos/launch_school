79052e2b59d51711cce6653fdbe68f007e0e3a3c
Oscar Cortes <cortes.oscar@outlook.com>
2020-06-23 09:06:53 -0700

    first commit for ex04 of return module

---
+++ rb100/ruby_basics/return/ex04.rb
@@ -0,0 +1,13 @@
+# ex04.rb
+# Breakfast, Lunch, or Dinner? (Part 4)
+
+# What will the following code print? Why? Don't run it until you've attempted 
+# to answer.
+
+def meal
+	puts 'Dinner'
+	return 'Breakfast'
+end
+  
+puts meal
+
