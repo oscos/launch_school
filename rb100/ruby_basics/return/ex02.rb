5083bf38df1d1c3d7f348ce72aef1b6c2fbe4534
Oscar Cortes <cortes.oscar@outlook.com>
2020-06-23 08:34:27 -0700

    first commit ex02 of return module

---
+++ rb100/ruby_basics/return/ex02.rb
@@ -0,0 +1,11 @@
+# ex01.rb
+# Breakfast, Lunch, or Dinner? (Part 1)
+
+# What will the following code print? Why? Don't run it until you've attempted 
+# to answer.
+
+def meal
+	'Evening'
+end
+  
+puts meal
