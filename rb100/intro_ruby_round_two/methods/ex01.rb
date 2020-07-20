e50693115e5e926ca82bfa4dc2c89c1a5a81e579
Oscar Cortes <cortes.oscar@outlook.com>
2020-07-20 05:41:11 +0000

    first commit

--- rb100/intro_ruby_round_two/methods/ex01.rb
+++ rb100/intro_ruby_round_two/methods/ex01.rb
@@ -1,3 +1,15 @@
 # Launch School - Intro to Ruby
 # Methods - ex01.rb
 # Answered On 07/19/2020
+
+def greeting(morning_or_noon, name, time)
+  puts "#{morning_or_noon} #{name}, nice to meet ya.  Today is #{time}!"
+end
+
+morning_or_noon = (0..11).include?(Time.now.hour) ? 'Good Morning' : 'Good Afternoon'
+time = Time.now.strftime("%A, %B %d, %I:%M %p")
+
+puts "Whats your name"
+input = gets.chomp.capitalize
+
+greeting(morning_or_noon, input, time)
