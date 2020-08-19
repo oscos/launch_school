afebfc69533938d0b2ac3ed48aa0c0950885039d
oscos <cortes.oscar@outlook.com>
2020-08-19 04:59:57 +0000

    first commit

---
+++ js100/basics/examples.js
@@ -0,0 +1,5 @@
+/* Launch School - Intro Javascript - Basics */
+
+console.log("He said, 'Hi there with single qoutes!'");
+console.log('He said, "Hi three with double qoutes!"');
+console.log(`5 plus 5 equals ${5 + 5}`);
