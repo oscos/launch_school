f333ff23b8f85113e30187687c55ea6e99dac77d
oscos <cortes.oscar@outlook.com>
2020-09-13 04:05:11 +0000

    first commit

---
+++ rb101/small_problems/easy1/x05.rb
@@ -0,0 +1,23 @@
+=begin
+Launch School: RB101-RB109 - Small ProblemsEasy 1 - Reverse It (Part 1)
+ExerciseName: [Reverse It (Part 1)(https://launchschool.com/exercises/51e98567)
+FileName: ex04.md
+Answered On: 09/12/2020
+=end
+
+# Write a method that takes one argument, a string, and returns a new string 
+# with the words in reverse order
+
+# require 'pry'
+
+def reverse_sentence(str)
+
+  str.split.reverse.join(" ")
+  
+end
+
+puts reverse_sentence('') == ''
+puts reverse_sentence('Hello World') == 'World Hello'
+puts reverse_sentence('Reverse these words') == 'words these Reverse'
+puts reverse_sentence('Reverse these words') == 'words these Reverse'
+
