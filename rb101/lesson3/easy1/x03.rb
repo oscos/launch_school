7aed80ca1bc6e37e6aa518cbd5c159f692d017ee
oscos <cortes.oscar@outlook.com>
2020-09-23 05:37:37 +0000

    lesson3 - easy - x03.rb first commit

---
+++ rb101/lesson3/easy1/x03.rb
@@ -0,0 +1,13 @@
+=begin
+Launch School: RB101 Programming Foundations - Lesson 3 - Practice Problems - Easy 1 - Question 3
+ExerciseName: [Question 3](https://launchschool.com/lessons/263069da/assignments/e2593fe1)
+FileName: ex03.rb
+Answered On: 09/22/2020
+=end
+
+# Replace the word "important" with "urgent" in this string:
+advice = "Few things in life are as important as house training your pet dinosaur."
+
+advice.sub!("important","urgent")
+
+p advice
