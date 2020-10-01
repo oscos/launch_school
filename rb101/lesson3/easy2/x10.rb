d01b7a58508b9274dc707e68aa63c51d60b471f1
oscos <cortes.oscar@outlook.com>
2020-10-01 03:09:22 +0000

    label modification

--- rb101/lesson3/easy2/x10.rb
+++ rb101/lesson3/easy2/x10.rb
@@ -1,8 +1,8 @@
 =begin
 Launch School: RB101 Programming Foundations - Lesson 3 - Practice Problems
 ExerciseName: [Easy 2](https://launchschool.com/lessons/263069da/assignments/86725689)
-FileName: ex06.rb
-Answered On: 09/24/2020
+FileName: ex10.rb
+Answered On: 09/29/2020
 =end
 
 # require "pry"
