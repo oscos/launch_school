6cb10f425b7725a2acdbe76bff97075fe0f242ec
oscos <cortes.oscar@outlook.com>
2020-10-12 02:42:17 +0000

    some mods

--- rb101/lesson3/hard1/x04.rb
+++ rb101/lesson3/hard1/x04.rb
@@ -19,11 +19,8 @@ def dot_separated_ip_address?(input_string)
   dot_separated_words = input_string.split(".")
   return false if dot_separated_words.size != 4
   
-  # p dot_separated_words
   while dot_separated_words.size > 0 do
      word = dot_separated_words.pop
-     # p word
-     # break unless true
      varx = is_an_ip_number?(word)
      break unless varx
   end
